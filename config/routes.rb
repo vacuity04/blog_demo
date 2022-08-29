0ab6ad567067d5b48c43d3275284ad400c85904a
vacuity04 <kncook@live.com>
2022-08-29 10:26:55 -0700

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get 'pages/home'
-  get 'pages/about'
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
